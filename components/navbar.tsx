2b95f0fab7e0f8313ae7865fd2f7dc193ef08de3
Anri Lombard <anri.m.lombard@gmail.com>
2022-07-21 17:03:55 +0200

    Navbar progress

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,34 +2,63 @@ import Link from 'next/link';
 
 function Navbar() {
     return (
-        <div className="navbar flex flex-row">
-            <div className="basis-1/4">
-                <Link href="/">
-                    Anri Lombard
+        // <div className="navbar flex flex-row">
+        //     <div className="basis-1/4 navbar-brand self-center hover:animate-pulse">
+        //         <Link href="/">
+        //             Anri Lombard
+        //         </Link>
+        //     </div>
+        //     {/* <div className=""></div> */}
+        //     <div className="basis-1/5 self-center navbar-item">
+        //         <Link href="/about">
+        //             About
+        //         </Link>
+        //     </div>
+        //     <div className="basis-1/5 self-center navbar-item">
+        //         <Link href="/learning">
+        //             Learning
+        //         </Link>
+        //     </div>
+        //     <div className="basis-1/5 self-center navbar-item">
+        //         <Link href="/projects">
+        //             Projects
+        //         </Link>
+        //     </div>
+        //     <div className="basis-1/5 self-center navbar-item">
+        //         <Link href="https://anri-lombard.github.io/">
+        //             Blog
+        //         </Link> and SM
+        //     </div>
+        // </div>
+
+        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+            <div className="container flex flex-wrap justify-between items-center mx-auto">
+                <Link href="/" className="flex items-center">
+                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Anri Lombard</span>
                 </Link>
+                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                </button>
+                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                        <li>
+                            <Link href="/about" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
+                        </li>
+                        <li>
+                            <Link href="/learning" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Learning</Link>
+                        </li>
+                        <li>
+                            <a href="/projects" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Projects</a>
+                        </li>
+                        <li>
+                            <a href="https://anri-lombard.github.io/" className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            {/* <div className="basis-1/4"></div> */}
-            <div className="basis-3/16">
-                <Link href="/about">
-                    About
-                </Link>
-            </div>
-            <div className="basis-3/16">
-                <Link href="/learning">
-                    Learning
-                </Link>
-            </div>
-            <div className="basis-3/16">
-                <Link href="/projects">
-                    Projects
-                </Link>
-            </div>
-            <div className="basis-3/16">
-                <Link href="https://anri-lombard.github.io/">
-                    Blog
-                </Link>
-            </div>
-        </div>
+        </nav>
+
     )
 }
 
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,17 @@
 import styles from "../styles/About.module.css"
 
 function About() {
-    return <div className="text-3xl font-bold underline">About</div>
+    return (
+        <div className="">
+            <div className={styles.profile}>
+                Box describing me in a fun way with animations
+            </div>
+            <div className={styles.codeBlock}>
+                Code with cool animation making it fun to learn about me - perhaps interactive<br />
+                Perhaps interactive and live coding
+            </div>
+        </div>
+    )
 }
 
 export default About
