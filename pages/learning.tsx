733e8baf5416053607fa506d6472355136c6aa35
Anri Lombard <anri.m.lombard@gmail.com>
2022-08-07 22:04:35 +0200

    lol

--- pages/learning.tsx
+++ pages/learning.tsx
@@ -1,14 +1,49 @@
-import PopupProfile from '../components/popupProfile';
+import { useState } from 'react';
 
 function Learning() {
-    const learning = {
+    const [learning, setLearning] = useState({
         "university": [
-            {"MAM1000W": "This was cool"},
-            {"MAM2000W": "This is still cool"},
-            {"STA1006S": "This was kinda nice"}
+            {
+                name: "MAM1",
+                description: "Indeed",
+                turned: false
+            },
+            {
+                name: "MAM2",
+                description: "Indeed",
+                turned: false
+            },
+            {
+                name: "MAM3",
+                description: "Indeed",
+                turned: false
+            },
         ]
+    })
+    interface ILearning {
+        name: string,
+        description: string,
+        turned: boolean
     }
-    
+
+    const flip = (course: ILearning) => {
+        const element = document.getElementById(course.name);
+        
+        if (element == null) {
+            return
+        }
+
+        // setLearning(!course.turned)
+
+        course.turned = !course.turned;
+        
+        element.classList.add("flip");
+        setTimeout(() => {
+            element.classList.remove("flip");
+        }, 500)
+        
+    }
+
 
     return (
         <div className="min-h-screen flex flex-col mx-10">
@@ -17,11 +52,22 @@ function Learning() {
             </div>
             <h3 className="heading">University</h3>
             <div className="box-container">
+                {
+                    learning['university'].map(course => (
+                        <button 
+                            key={course.name} 
+                            id={course.name} 
+                            className="box"
+                            onClick={() => flip(course)}
+                        >{course.turned ? course.description : course.name}</button>
+                    ))
+                }
+                {/* <button className="box flipper"></button>
                 <div className="box flipper"></div>
                 <div className="box"></div>
-                <div className="box"></div>
+                <div className="box"></div> */}
             </div>
-            
+
             <h3 className="heading">Outside University</h3>
             <div className="box-container">
                 <div className="box"></div>
