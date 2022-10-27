ae969b8a94f528a35594841483bb12e1dbb41747
Anri Lombard <anri.m.lombard@gmail.com>
2022-10-27 21:28:38 +0200

    Contacts info

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,6 +7,20 @@ function Contact() {
             <div>
                 Hard-to-reach kinda dude... but try lmbanr001@myuct.ac.za if you feel adventurous, or visit your local librarian and ask for the weird white kid that shows up on Sundays.
             </div>
+            <div>
+                Otherwise, if you're really eager, just click <span className="dissapear">HERE</span>.
+                <details>
+                    <summary>If you are Hermione...</summary>
+                    I can't give you my number like this, but here's the plan:
+                    1. Get into my uni,
+                    2. Ask for a super short blonde chad that is obviously a lady's man,
+                    3. Now don't go to whomever they mentioned,
+                    4. Repeat
+
+                    Eventually you'll find me :)
+                </details>
+            </div>
+            
         </div>
     )
 }
