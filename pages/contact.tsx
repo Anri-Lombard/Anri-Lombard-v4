2edce77ba3872e3ed95d9d15cb004a71a42c3a25
Anri Lombard <anri.m.lombard@gmail.com>
2022-11-17 09:00:06 +0200

    Fixes

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,7 +5,7 @@ function Contact() {
                 Contact
             </div>
             <div className="contact">
-                I'm probabaly studying or reading most of the time, but if you feel adventurous, you can try to contact me at:
+                I&aposm probabaly studying or reading most of the time, but if you feel adventurous, you can try to contact me at:
                 <div className="font-bold text-lg">
                     <a href="mailto:LMBANR001@myuct.ac.za">LMBANR001@myuct.ac.za</a>
                 </div>
