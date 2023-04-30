be73b90e079110ce46a6b59511293b542f2ae952
Anri Lombard <anri.m.lombard@gmail.com>
2023-04-30 16:25:48 +0200

    Update copyright automatically

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,20 +1,11 @@
-import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faGithub,
-    faLinkedin
-} from "@fortawesome/free-brands-svg-icons";
-import {
-    faCircleHalfStroke
-} from "@fortawesome/free-solid-svg-icons";
-
 function Footer() {
+    const currentYear = new Date().getFullYear();
+
     return (
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 opacity-50">
             <div className="p-10 flex flex-col">
                 <span className="flex-1 self-center">
-
-                    &copy; 2022 Anri Lombard
+                    &copy; {currentYear} Anri Lombard
                 </span>
             </div>
         </nav>
