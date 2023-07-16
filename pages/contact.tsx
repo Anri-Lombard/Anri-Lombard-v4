ec5b9e2d5e0cc979f7d69af20f9d5304309993a6
Anri Lombard <anri.m.lombard@gmail.com>
2023-07-16 18:57:34 +0200

    Contact update

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -13,10 +13,10 @@ function Contact() {
                 </div>
                 <div className="contact max-w-2xl mt-10">
                     <p className="mb-5">
-                        Hey there! When I&lsquo;m not busy diving into the world of Machine Learning or studying Computer Science and Mathematical Statistics, I make sure to stay connected.
+                        Hey there! When I&lsquo;m not doing Machine Learning or studying Computer Science and Mathematical Statistics, I make sure to stay connected.
                     </p>
                     <p className="mb-5">
-                        So, if you want to discuss a project, share insights, or just chat about coding and statistics, don&lsquo;t hesitate to reach out. Keep in mind that I might take a day or two to respond, as I&lsquo;m often engaged in the delightful dance of coding and studying.
+                        So, if you want to discuss a project, share insights, or just chat about coding and statistics, don&lsquo;t hesitate to reach out. Keep in mind that I might take some days to respond.
                     </p>
                     <p>
                         My school email address is{" "}
@@ -25,7 +25,12 @@ function Contact() {
                                 lmbanr001@myuct.ac.za
                             </a>
                         </strong>
-                        .
+                         and my personal email address is{" "}
+                        <strong>
+                            <a href="mailto:anri.m.lombard@gmail.com" className="text-blue-600 hover:text-blue-800 transition duration-300">
+                                anri.m.lombard@gmail.com
+                            </a>
+                        </strong>
                     </p>
                 </div>
             </div>
